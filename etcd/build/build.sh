8d57b670c5fbe870ee2813e84b2091633b455caf
wenxilu <wenxilu@foxmail.com>
2022-06-07 22:31:09 +0800

    build init

--- etcd/build/build.sh
+++ etcd/build/build.sh
@@ -1,5 +1,20 @@
-wget cfssl https://github.com/cloudflare/cfssl/releases/download/v1.6.1/cfssl-bundle_1.6.1_linux_amd64
-wget https://github.com/etcd-io/etcd/releases/download/v3.5.4/etcd-v3.5.4-linux-amd64.tar.gz
-docker build -t konomo/etcd/etcd:v1 . -f Dockerfile
+#!/bin/bash
+# wget cfssl https://github.com/cloudflare/cfssl/releases/download/v1.6.1/cfssl-bundle_1.6.1_linux_amd64
 
-mkdir -p /data/etcd-0{1..3}
+package_dir=./packages
+download_url=https://github.com/etcd-io/etcd/releases/download/v3.5.4/etcd-v3.5.4-linux-amd64.tar.gz
+package_name=etcd-v3.5.4-linux-amd64.tar.gz
+
+if [ ! -d ${package_dir} ];then
+    mkdir -p ${package_dir}
+fi
+
+if [ ! -e ${package_dir}/${package_name} ];then
+    wget -P ${package_dir} ${download_url}
+fi
+
+docker build --rm --no-cache -t konomo/etcd/etcd:v1 . -f Dockerfile
+# mkdir -p /data/etcd-0{1..3}
+
+# 清理 dangling image
+docker rmi $(docker images -f "dangling=true" -q)
--- etcdkeeper/build/build.sh
+++ etcdkeeper/build/build.sh
@@ -1,13 +1,17 @@
 package_dir=./packages
-etcdkeeper_download_url=https://github.com/evildecay/etcdkeeper/releases/download/v0.7.6/etcdkeeper-v0.7.6-linux_x86_64.zip
-etcdkeeper_package_name=etcdkeeper-v0.7.6-linux_x86_64.zip
+download_url=https://github.com/evildecay/etcdkeeper/releases/download/v0.7.6/etcdkeeper-v0.7.6-linux_x86_64.zip
+package_name=etcdkeeper-v0.7.6-linux_x86_64.zip
 
 if [ ! -d ${package_dir} ];then
     mkdir -p ${package_dir}
 fi
 
-if [ ! -e ${package_dir}/${etcdkeeper_package_name} ];then
-    wget -P ${package_dir} ${etcdkeeper_download_url}
+if [ ! -e ${package_dir}/${package_name} ];then
+    wget -P ${package_dir} ${download_url}
 fi
 
-docker build -t konomo/etcd/etcdkeeper:v1 . -f Dockerfile
+docker build --rm -t konomo/etcd/etcdkeeper:v1 . -f Dockerfile
+
+
+# 清理 dangling image
+docker rmi $(docker images -f "dangling=true" -q)
